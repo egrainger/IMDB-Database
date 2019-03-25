7db287d056bb7b22276c363dd9136325cc0310f2
Emma G <egrainger@crimson.ua.edu>
2019-03-24 23:25:38 -0500

    Implement delete
    Source code didn't implement a delete node -___- (angry face)
    Trying my own so far

--- avltree.c
+++ avltree.c
@@ -1,5 +1,9 @@
-/* Code courtesy of http://www.zentut.com/c-tutorial/c-avl-tree/ */
-//Code updated to work with my needs
+/* Code courtesy of http://www.zentut.com/c-tutorial/c-avl-tree/ 
+    https://www.techiedelight.com/find-inorder-successor-given-key-bst/
+    https://stackoverflow.com/questions/10988677/deleting-an-element-in-avl-trees-in-c
+    https://www.thecrazyprogrammer.com/2014/03/c-program-for-avl-tree-implementation.html
+    Updated to work with my needs
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,7 +23,6 @@ void dispose(node* t) {
 }
 
 /*find a specific node's title in the tree*/
-//struct Movie find(char *str, node* t ) {
 node *find(char *str, node*t){ 
   int len;
   len = strlen(str); //Useful for strncmp() which will be useful for full or partial matches 
@@ -178,22 +181,126 @@ node* insert(struct Movie newMovie, node* t ) {
   /* Else X is in the tree already; we'll do nothing */
   
     t->height = max( height( t->left ), height( t->right ) ) + 1;
-    //printf("%s\n", t->info.Title);
+    
     return t;
+}
+
+/*Find the successor node*/
+node* successor(char *str, node *t, node* succ) {
+  //base case
+  if (t == NULL) {
+    succ = NULL;
+    return succ; 
+  }
+
+  //if node with key's value is found, the successor is min value node in its right subtree (if any)
+  if (strcmp(str, t->info.avlTitle) == 0) {
+    if (t->right)
+      succ = find_min(t->right);
+  }
+
+  //if given key is less than the root node recurse for left subtree
+  else if (strcmp(str, t->info.avlTitle) < 0) {
+    //update successor to current node before recursing left subtree
+    succ = t;
+    successor(str, t->left, succ); 
+  }
+  //if given key is more than the root node, recurse for right subtree
+  else
+    successor(str, t->right, succ); 
+}
+
+int balance_factor(node *t)
+{
+	int lh,rh;
+	if(t == NULL)
+	  return(0);
+	
+	if(t->left == NULL)
+	  lh = 0;
+	else
+	  lh = 1 + t->left->height;
+	
+	if(t->right == NULL)
+	  rh = 0;
+	else
+	  rh = 1 + t->right->height;
+	
+	return(lh-rh);
 }
 
 /*remove a node in the tree*/
 //FIXME
-node* delete( char* str, node* t ) {
-    printf( "Sorry; Delete is unimplemented; %s remains\n", str );
+node* delete(node* t, char *str ) {
+  struct node *remove_node;
+  //If empty can't delete
+  if (t == NULL){
     return t;
+  }
+  
+  //Check to find stuff
+  if (strcmp(str, t->info.avlTitle) < 0) {
+    t->left = delete(t->left, str);
+    
+  } else if (strcmp(str, t->info.avlTitle) > 0) {
+    t->right = delete(t->right,str);
+  }
+  else {
+
+    if ((t->left == NULL) && (t->right != NULL)){
+      remove_node = t->right;
+      *t = *remove_node;
+      dispose(remove_node); // this is for free-ing the memory
+      
+    } else if ((t->right == NULL)  && (t->left != NULL)){
+      remove_node = t->left;
+      *t = *remove_node;
+      dispose(remove_node);
+	    
+    } else if ((t->right == NULL)  && (t->left == NULL)){
+      remove_node = t;
+      t = NULL;
+      
+    } else {
+      node* succ = NULL; 
+      remove_node = successor(str, t, succ);
+      t->info.avlTitle = remove_node->info.avlTitle;
+      t->right = delete(t->right, remove_node->info.avlTitle);
+    }
+
+    }
+
+    if (t == NULL) {
+        return t;
+
+    if (balance_factor(t) == 2){
+        if (balance_factor(t->left) == -1) {
+	  return single_rotate_with_right(t); 
+
+        } else if (balance_factor(t->left) == 1) {
+	  return double_rotate_with_left(t); 
+        }
+    }
+
+    if (balance_factor(t) == -2) {
+        if (balance_factor(t->right) == -1) {
+	  return single_rotate_with_left(t); 
+	}
+
+	else if (balance_factor(t->right) == 1)  { 
+	  return double_rotate_with_right(t); 
+            }
+        }
+    }
+
+    return t;    
 }
 
-/*get data of a node*/
-//FIXME
+  
+/*get data of a node
 char* get(node* n) {
     return n->info.Title;
-}
+}*/
 
 /*Recursively display AVL tree or subtree*/
 void display_avl(node* t) {
@@ -207,6 +314,7 @@ void display_avl(node* t) {
   display_avl(t->right);
 }
 
+/*Recursively print all matches for the user's input and save them to an array*/
 void display_matches(node* t, char *userInput, struct Movie *matches) { 
   static int counter = 1;
   printf("counter value is: %d\n", counter); 
@@ -233,7 +341,6 @@ void display_matches(node* t, char *userInput, struct Movie *matches) {
     strcpy(matches[index].releaseYear,t->info.releaseYear);
     strcpy(matches[index].runtimeMinutes,t->info.runtimeMinutes);
     strcpy(matches[index].genres,t->info.genres);
-    printf("Genres: %s", t->info.genres); 
     strcpy(matches[index].avlTitle,t->info.avlTitle);
     printf("%s\t%s\t%s\t%s\n",t->info.Title, t->info.releaseYear, t->info.runtimeMinutes, t->info.genres);
   }
